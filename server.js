911c3e878cc95b7ba6f458d9dab731990e89a056
Merima Ceranic <merimaceranic98@gmail.com>
2022-04-08 13:54:00 +0200

    Added cors to the project.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 
 server.db = router.db;
 
+server.use(cors());
 server.use(auth);
 server.use(router);
-server.listen(3500)
+server.listen(3500);
